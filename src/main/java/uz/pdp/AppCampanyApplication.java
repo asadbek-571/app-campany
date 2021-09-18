04bcd1fab26d17998da24c1c99e885c268b2082a
Asadbek-Kushakov <76949935+Asadbek-Kushakov@users.noreply.github.com>
2021-09-18 16:32:26 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/AppCampanyApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCampanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCampanyApplication.class, args);
+    }
+
+}
